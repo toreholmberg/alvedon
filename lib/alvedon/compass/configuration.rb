444fc2b885c621cf68b950e8ee351815edb961e1
Tore Holmberg <tore.holmberg@gmail.com>
2013-04-23 19:48:48 -0400

    better compass configuration register

--- lib/alvedon/compass/configuration.rb
+++ lib/alvedon/compass/configuration.rb
@@ -8,21 +8,22 @@ module Alvedon
     class Configuration
 
       # TODO: add more attrs
-      attr_accessor :images_dir, :javascripts_dir, :fonts_dir
+      attr_accessor :images_dir, :javascripts_dir, :fonts_dir, :line_comments
 
       def initialize *args
         # defaults
         @images_dir = 'images'
         @javascripts_dir = 'javascripts'
         @fonts_dir = 'fonts'
+        @line_comments = true
       end
 
       def register
-        hash = Hash.new
-        hash[:images_dir] = @images_dir
-        hash[:javascripts_dir] = @javascripts_dir
-        hash[:fonts_dir] = @fonts_dir
-        ::Compass.add_configuration hash, 'Alvedon'
+        config = Hash.new
+        instance_variables.each do |var|
+          config[var.to_s.gsub(/[@]/, '')] = instance_variable_get var
+        end
+        ::Compass.add_configuration config, 'Alvedon'
       end
     end
   end
