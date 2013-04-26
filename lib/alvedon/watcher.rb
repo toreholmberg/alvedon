cd74a37012c015ae2274dce91de5662222be8138
Tore Holmberg <tore.holmberg@gmail.com>
2013-04-26 17:43:41 -0400

    trying forced polling

--- lib/alvedon/watcher.rb
+++ lib/alvedon/watcher.rb
@@ -51,7 +51,8 @@ module Alvedon
       Listen.to(*paths)
         .filter(*filters)
         .change(&callback)
-        .start!
+        .force_polling(true)
+        .start
     end
 
     private
